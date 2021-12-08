4cf38e3063196a679b86252d4f16876e64424cee
viet.ha <viet.ha@amela.vn>
2021-12-08 18:55:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react'
+import { Routes, Route, Link} from 'react-router-dom'
+import Home from './component/Home';
+import DndComponent from './component/Dnd/DndComponent';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Select the route that you want </h1>
+      <ul>
+        <li><a href="/dnd"> drag and drop</a></li>
+        <li><a href="/crop">crop image</a></li>
+      </ul>
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route exact path="dnd" element={<DndComponent />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/component/Dnd/DndComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DndComponent() {
+    return (
+        <div>
+           this is dnd component asdasdfasf
+        </div>
+    )
+}
+
+export default DndComponent
